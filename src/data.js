8782b902c1d1fdfc4eb62b0fb8743102a3b7129f
EdwinSch <edwinschutjes@gmail.com>
2023-10-27 11:52:21 +0200

    prj init

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const summary = [
+  {
+    id: 1,
+    usp: "Track company-wide progress",
+    description:
+      "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.",
+  },
+  {
+    id: 2,
+    usp: "Advanced built-in reports",
+    description:
+      "Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+  },
+  {
+    id: 3,
+    usp: "Everything you need in one place",
+    description:
+      "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+  },
+];
+
+export const testimonials = [
+  {
+    id: 1,
+    name: "Anisha Li",
+    text: "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.",
+  },
+  {
+    id: 2,
+    name: "Ali Bravo",
+    text: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.",
+  },
+  {
+    id: 3,
+    name: "Richard Watts",
+    text: "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!",
+  },
+  {
+    id: 4,
+    name: "Shanai Gough",
+    text: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.",
+  },
+];
