cd9ebdaf95ce0f4b6a289f63469d88cd38d43382
EdwinSch <edwinschutjes@gmail.com>
2023-11-03 14:07:04 +0100

    slider functionality manual setup

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react";
+import { testimonials } from "../../data";
+import Button from "../Button/Button";
+
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
 const Slider = () => {
+  const [people, setPeople] = useState(testimonials);
+  const [currentPerson, setCurrentPerson] = useState(0);
+
+  const prevSlide = () => {
+    if (currentPerson === 0) {
+      setCurrentPerson(people.length - 1);
+      return;
+    }
+    setCurrentPerson(currentPerson - 1);
+  };
+
+  const nextSlide = () => {
+    if (currentPerson === people.length - 1) {
+      setCurrentPerson(0);
+      return;
+    }
+    setCurrentPerson(currentPerson + 1);
+  };
+
   return (
     <section className="slider-container">
-      <p>slider</p>
+      <h4>What they've said</h4>
+
+      {/* TEMP --- */}
+      <div className="btns">
+        <button onClick={prevSlide} className="btn-chevron" type="button">
+          <FaChevronLeft />
+        </button>
+        <button onClick={nextSlide} className="btn-chevron" type="button">
+          <FaChevronRight />
+        </button>
+      </div>
+
+      {/* --- TEMP */}
+
+      <div className="carrousel-wrapper">
+        {people.map((person, index) => {
+          //destructure
+          const { id, image, name, quote } = person;
+
+          return (
+            <article
+              className="slide"
+              key={id}
+              style={{
+                transform: `translateX(${100 * (index - currentPerson)}%)`,
+              }}
+            >
+              {/* <img src={image} alt="avatar" className="avatar" /> */}
+              <p className="name">{name}</p>
+              <p className="body-text">{quote}</p>
+            </article>
+          );
+        })}
+      </div>
+
+      <Button variant={"btn-primary"} />
     </section>
   );
 };
--- src/data.jsx
+++ src/data.jsx
@@ -47,25 +47,29 @@ export const testimonials = [
     id: 1,
     image: personA,
     name: "Anisha Li",
-    text: "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.",
+    quote:
+      "Manage has supercharged our team’s workflow. The ability to maintain visibility on larger milestones at all times keeps everyone motivated.",
   },
   {
     id: 2,
     image: personB,
     name: "Ali Bravo",
-    text: "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.",
+    quote:
+      "We have been able to cancel so many other subscriptions since using Manage. There is no more cross-channel confusion and everyone is much more focused.",
   },
   {
     id: 3,
     image: personC,
     name: "Richard Watts",
-    text: "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!",
+    quote:
+      "Manage allows us to provide structure and process. It keeps us organized and focused. I can’t stop recommending them to everyone I talk to!",
   },
   {
     id: 4,
     image: personD,
     name: "Shanai Gough",
-    text: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.",
+    quote:
+      "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in-sync without being intrusive.",
   },
 ];
 
